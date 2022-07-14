332c83491d25cebfa5193a783ba4956753eca68b
skylersaucedo <skyler.saucedo@gmail.com>
2022-07-14 12:59:11 -0500

    added dropdowns

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { AppHeader } from '@corva/ui/components';
 import PropTypes from 'prop-types';
 import { DEFAULT_SETTINGS } from './constants';
+import { UserInputSection } from './assets/components/user_input'
 
 import logo from './assets/logo.svg';
 import styles from './App.css';
@@ -11,26 +12,7 @@ function App(props) {
     <div className={styles.container}>
       <AppHeader {...appHeaderProps} />
       <div className={styles.content}>
-        <div>
-          <img src={logo} alt="logo" className={styles.logo} />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-            <br />
-            <br />
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </div>
-        <div>
-          Settings &quot;Example&quot; checkbox is{' '}
-          {isExampleCheckboxChecked ? 'checked' : 'unchecked'}
-        </div>
+        <UserInputSection />  
       </div>
     </div>
   );
